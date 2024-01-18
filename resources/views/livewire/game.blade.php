2114db4dee18e3af27da50ef6c904f079dcb101b
Liam Duckett <liamduckett2@gmail.com>
2024-01-18 19:51:38 +0000

    display basic card shape, instead of list

--- resources/views/livewire/game.blade.php
+++ resources/views/livewire/game.blade.php
@@ -2,13 +2,23 @@
     /** @var array<\App\Models\Hand> $hands  */
 @endphp
 
-<div>
+<div class="max-w-5xl mx-auto flex flex-col gap-20">
     @foreach($hands as $hand)
-        <ul>
+        <ul class="flex flex-wrap gap-4">
             @foreach($hand->cards as $card)
-                <li>
-                    {{ $card }}
-                </li>
+                <div class="border-2 border-black rounded-3xl w-36 h-48 flex flex-col justify-between p-2">
+                    <div class="text-left">
+                        {{ $card->rank->value }}
+                    </div>
+
+                    <div class="text-center">
+                        {{ $card->suit->value }}
+                    </div>
+
+                    <div class="text-right">
+                        {{ $card->rank->value }}
+                    </div>
+                </div>
             @endforeach
         </ul>
     @endforeach
