339d29d4ae72e7c310def090572341fec613f3b9
Liam Duckett <liamduckett2@gmail.com>
2024-01-20 12:35:24 +0000

    display a very basic board

--- resources/views/livewire/game.blade.php
+++ resources/views/livewire/game.blade.php
@@ -1,9 +1,25 @@
 @php
     /** @var array<\App\Models\Hand> $hands  */
     /** @var int $currentPlayer  */
+    /** @var \App\Models\Board $board  */
 @endphp
 
 <div class="max-w-5xl mx-auto flex flex-col gap-20 py-10">
+    <div class="flex gap-4">
+        @foreach($board->contents as $name => $boardSuit)
+            <div class="">
+                <div>
+                    {{ $name }}
+                </div>
+
+                <div>
+                    <div>{{ $boardSuit->lowest ?? 'N/A' }}</div>
+                    <div>{{ $boardSuit->highest ?? 'N/A' }}</div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
     @foreach($hands as $player => $hand)
         <ul class="flex flex-wrap gap-3 border-4 p-8 rounded-xl justify-center
             {{ $player === $currentPlayer ? 'border-blue-400' : 'border-transparent' }}">
