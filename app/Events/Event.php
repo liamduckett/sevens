c2809b7c3a484a9758264e4eee4d9874c0c77a27
Liam Duckett <liamduckett2@gmail.com>
2024-01-23 18:45:40 +0000

    cleanup events

---
+++ app/Events/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Event implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Get the channels the event should broadcast on.
+     */
+    public function broadcastOn(): Channel
+    {
+        return new Channel('lobby');
+    }
+}
--- app/Events/PlayerLeft.php
+++ app/Events/PlayerLeft.php
@@ -10,27 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PlayerLeft implements ShouldBroadcast
+class PlayerLeft extends Event
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new Channel('lobby'),
-        ];
-    }
+    //
 }
